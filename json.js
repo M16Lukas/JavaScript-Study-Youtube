6e0c38107edbbcbf0318e88d53fc7799005487a4
Park <46748131+M16Lukas@users.noreply.github.com>
2021-02-16 17:34:17 +0900

    Create json.js

---
+++ json.js
@@ -0,0 +1,49 @@
+// JSON
+// Javascript Object Notation
+
+// 1. Object to JSON
+// stringfy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+  name: 'tori',
+  color: 'white',
+  size: null,
+  birthDate: new Date(),
+  jump: () => {
+    console.log(`${this.name} can jump`);
+  }
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ['name']);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key: ${key} value: ${value}`);
+  return key === 'name' ? 'ellie' : value;
+});
+console.log(json);
+
+
+console.clear()
+// 2. JSON to Object
+// parse(json)
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+  console.log(`key: ${key} value: ${value}`);
+  return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();  // return 'can jump!'
+// obj.jump();  // return Error
+
+console.log(rabbit.birthDate.getDate());  // return 16 (Number)
+console.log(obj.birthDate.getDate());     // return 16 (Number)
+
